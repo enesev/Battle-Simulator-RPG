d4cb966c65c8a2ecc215f7be0ccc9386aaae6aa8
nilsoriano <nilsorianovila@gmail.com>
2022-08-09 22:11:30 +0200

    Método auxiliar incluido para comprobar el correcto funcionamiento de esta clase

--- src/Graveyard.java
+++ src/Graveyard.java
@@ -11,7 +11,16 @@ public class Graveyard {
         setDeadCharacters(deadCharacters);
     }
 
-
+    // método temporal, no pertenece a la versión final, debería ser implementado en otra clase
+    public void ifCharacterDied_addToGraveyard(Character character, Graveyard graveyard) {
+        if (character.getHp() <= 0) {
+            character.setAlive(false);
+            graveyard.addDeadCharacter(getDeadCharacters(),character);
+            graveyard.showGraveyard();
+        } else {
+            //no ha muerto, no hacer nada
+        }
+    }
     // metodos
     /**
     * Añade un carácter a la lista del cementerio. Cuando isAlive pase a false, se llama a este método:
@@ -28,7 +37,10 @@ public class Graveyard {
     }
 
     public void showGraveyard () {
-        getDeadCharacters().forEach(System.out::println);
+        System.out.println("Carácteres en el cementerio:");
+        for (Character c:getDeadCharacters()) {
+            System.out.println(c.getName());
+        }
     }
 
     // getter
@@ -40,3 +52,15 @@ public class Graveyard {
         this.deadCharacters = deadCharacters;
     }
 }
+
+/*
+He ejecutado esta serie de métodos en Main (sin guardarlos ni commitearlos) para comprobar el correcto funcionamiento
+de esta clase. El resultado es el esperado: crea un wizard y una lista vacía de characters, con la que construye
+el cementerio. Mato al wizard usando los setters de Character, el método auxiliar verifica su muerte
+y lo añade al cementerio
+Wizard wiz1 = new Wizard(1337, "deadWizard",1,0,10);
+List<Character> deadChars = new ArrayList<Character>(){};
+Graveyard graveyard = new Graveyard(deadChars);
+wiz1.setHp(wiz1.getHp()-2);
+graveyard.ifCharacterDied_addToGraveyard(wiz1, graveyard);
+ */
