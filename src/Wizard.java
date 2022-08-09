9a0e5eef6d7cc0943aeb6e983c5d57f28ff35d98
sergiolara92 <sergioldm92@gmail.com>
2022-08-09 20:41:47 +0200

    modificacion 9/8

--- src/Wizard.java
+++ src/Wizard.java
@@ -10,28 +10,53 @@ public class Wizard extends Character implements Attacker {
         this.intelligence = intelligence;
     }
 
+    public int getMana() {
+        return mana;
+    }
+
+    public void setMana(int mana) {
+        this.mana = mana;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
     @Override
     public void attack() {
 
-        if (mana >= 5) {
+        if (this.getMana() >= 5) {
             fireball();
         } else {
             staffHit();
         }
     }
 
-    public void fireball(){
-        int damage = intelligence;
+    public int fireball(){
+        int damage = this.getIntelligence();
         int manaDecrease = 5;
-        this.mana = this.mana + manaDecrease;
-
-        //              este volumen + el nuevo, es mas grande que max? si es que si, sera MAX, sino sera este volume+volume nuevo
-
+        this.setMana(this.getMana() - manaDecrease);
+        return damage;
     }
+
 
     public void staffHit(){
         int damage = 2;
         int manaIncrease = 1;
         this.mana = this.mana + manaIncrease;
     }
+
+    public void decreaseHealth(Character player){
+        player.setHp(player.getHp()-fireball());
+    }
+
+
+
+
 }
+
+
